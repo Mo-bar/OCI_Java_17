80070dbb1fd9b270c9a3dd7db119704a1f56699d
Barkouch Mourad <mourad.barkouch@emsi-edu.ma>
2023-10-07 13:44:30 +0100

    Java Compiler program

---
+++ javaCompilerProgram/src/pkg/Test.java
@@ -0,0 +1,11 @@
+package pkg;
+
+//import java.lang.*;
+
+public class Test {
+	public static void main(String[] args){
+		for(String name : args){
+			System.out.println("Hello, Mr " +name);
+		}
+	}
+}
