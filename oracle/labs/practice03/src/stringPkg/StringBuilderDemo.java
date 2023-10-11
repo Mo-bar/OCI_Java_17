b21024dc58dd865a8f07579c33ea53c95dc11464
Barkouch Mourad <mourad.barkouch@emsi-edu.ma>
2023-10-11 15:49:26 +0100

    chapter 3

---
+++ oracle/labs/practice03/src/stringPkg/StringBuilderDemo.java
@@ -0,0 +1,23 @@
+package stringPkg;
+
+import java.lang.StringBuilder;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("demo");
+        System.out.println(stringBuilder);
+        stringBuilder.append("s");
+        System.out.println(stringBuilder);
+        stringBuilder.delete(2,4);
+        System.out.println(stringBuilder);
+        stringBuilder.reverse();
+        System.out.println(stringBuilder);
+        System.out.println(stringBuilder.capacity());
+    }
+}
+
+/**
+ * javac -cp classes/ -d classes/ src/stringPkg/StringBuilderDemo.java
+ * java -cp classes/ stringPkg.StringBuilderDemo
+ */
