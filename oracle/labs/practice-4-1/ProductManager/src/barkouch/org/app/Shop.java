8e3cbbd8ee67a9b5c7a5c3ad4bbfd7b61318af22
root <root@Mo>
2023-10-17 16:55:08 +0100

    part 2 of chapter 4

--- oracle/labs/practice-4-1/ProductManager/src/barkouch/org/app/Shop.java
+++ oracle/labs/practice-4-1/ProductManager/src/barkouch/org/app/Shop.java
@@ -1,10 +1,4 @@
-/*
- * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
+
 
 package barkouch.org.app;
 
@@ -13,6 +7,8 @@ import barkouch.org.data.Product;
 import java.math.BigDecimal;
 
 /**
+ * {@code Shop} class represents an application that manages Products.
+ * @version 17.8.1
  * @author BarkouchMourad
  **/
 public class Shop {
@@ -20,10 +16,11 @@ public class Shop {
         Product product = new Product();
         product.setId(101);
         product.setName("Tea");
-        product.setPrice(BigDecimal.valueOf(1.99));
+        product.setPrice(BigDecimal.valueOf(3.99));
 
         System.out.println("Id : "+product.getId());
         System.out.println("Name : "+product.getName());
         System.out.println("Price : "+product.getPrice());
+        System.out.println("Discount : "+product.getDiscountRate());
     }
 }
--- oracle/labs/practice-4-1/ProductManager/src/barkouch/org/data/Product.java
+++ oracle/labs/practice-4-1/ProductManager/src/barkouch/org/data/Product.java
@@ -1,14 +1,31 @@
 package barkouch.org.data;
 
 import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_DOWN;
 
 /**
+ * {@code Product} class represents properties and behaviours
+ * of product objects in the Product Management System.
+ * <br>
+ * Each product has an id, name, price.
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link java.math.BigDecimal DISCOUNT_RATE }
+ * @version 17.8.1
  * @author BarkouchMourad
  **/
 public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+
+    /**
+     * A constant that defines a
+     * {@link java.math.BigDecimal BigDecimal } value of the discount rate
+     * <br>
+     * Discount rate is 10%.
+     */
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(.1);
 
     public Product() {
     }
@@ -35,5 +52,16 @@ public class Product {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    /**
+     * Calculates discount based on a product price and
+     * {@code  DISCOUNT_RATE discount rate}
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal}
+     * value of the discount
+     */
+    public BigDecimal getDiscountRate(){
+    return price.multiply(DISCOUNT_RATE).setScale(2, HALF_DOWN);
     }
 }
